48bc5d818b7d5d5c32ef0609145d6b78d4f4d83e
kjell <kjell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-05-13 09:01:39 +0000

    Changed package name and fixed (I think) Makevars.win.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lpsolve@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/src/RlpSolve.h
+++ trunk/src/RlpSolve.h
@@ -4,15 +4,8 @@
 #include <R_ext/Rdynload.h>
 #include <R_ext/Utils.h>
 
-/* lpSolve initialization routine - automatically called by library.dynam */
-
 void R_init_lpSolve(DllInfo *info);
-
-
-/* Safe accessor method for lprec objects */
-
 lprec* lprecPointerFromSEXP(SEXP Slprec);
 int __WINAPI RlpSolveAbortFunction(lprec *lp, void *userhandle);
-SEXP RlpSolve_is_bound(SEXP Sobject);
 
 
